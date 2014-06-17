3c5cb7b0b5b435e9f5bff86d5810560ab5c1a6a8
sangwook <2sangwook@gmail.com>
2014-06-17 17:26:41 +0900

    init

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ aws-s3-bash-upload
 ==================
 
 you can upload file to aws s3 using bash, openssl, and curl.
+
+## Usage
+
+```
+$ ./aws_s3_bash_upload.sh your-aws-key-xxxxxxx your-aws-secretkey-XXXXXXXXXXXXXXXXXXXXX your-bucket-name /to/upload/file/tmp.dump s3-ap-northeast-1.amazonaws.com
+```
---
+++ aws_s3_bash_upload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+s3Key=$1
+s3Secret=$2
+bucket=$3
+file=$4
+host=$5
+resource="/${bucket}/${file}"
+contentType="application/x-compressed-tar"
+dateValue=`date -R`
+stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+curl -X PUT -T "${file}" \
+  -H "Host: ${bucket}.s3.amazonaws.com" \
+  -H "Date: ${dateValue}" \
+  -H "Content-Type: ${contentType}" \
+  -H "Authorization: AWS ${s3Key}:${signature}" \
+  https://${bucket}.${host}/${file}
